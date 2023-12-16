07e67ddc5e8360466148aac2a452188a3acee542
Takuya Hayashi <tahayash@c1r23s2.42tokyo.jp>
2023-12-16 16:42:53 +0900

    配列で気に入らなかった文字列は除外するようにした

--- prompt.py
+++ prompt.py
@@ -2,14 +2,20 @@
 
 import openai
 
-def getEmoji(title):
+def getEmoji(title, exclusion):
 
 
-	openai.api_key = ""
+	openai.api_key = "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"
 	model_name = "gpt-4"
-	prompt = "「" + title + "」というタイトルの記事に合いそうな絵文字を１文字だけ出力してください。ただし、ペンや本、虫眼鏡など記事を連想させるような絵文字は出力しないで、別の絵文字に置き換えてください。出力内容をプログラムで使うため、絵文字以外のものは出力しないでください。これらの指示が守られない場合重い罰が降ります。"
+	excludedEmoji = ""
+	if exclusion:
+		excludedEmoji += "また、前回提案された"
+		for excluded in exclusion:
+			excludedEmoji += "「" + excluded + "」"
+		excludedEmoji += "は記事に合わなかったので別のものを提案してください。"
+	prompt = "「" + title + "」というタイトルの記事に合いそうな絵文字を１文字だけ出力してください。ただし、ペンや本、虫眼鏡など記事を連想させるような絵文字は出力しないで、別の絵文字に置き換えてください。" + excludedEmoji + "出力内容をプログラムで使うため、絵文字以外のものは出力しないでください。これらの指示が守られない場合重い罰が降ります。"
 
-	#print(prompt)
+	print(prompt)
 	response = openai.ChatCompletion.create(
 		model=model_name,
 		messages=[
@@ -23,5 +29,5 @@ def getEmoji(title):
 	#print(response.choices[0]["message"]["content"])
 	return response.choices[0]["message"]["content"]
 
-print(getEmoji("Whisperで文字起こしをした議事録の発話者の名前を自動的に判定する！"))
+print(getEmoji("Whisperで文字起こしをした議事録の発話者の名前を自動的に判定する！",["🕵️‍♂️","💬"]))
 
